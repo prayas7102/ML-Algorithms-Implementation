c3f3fc28c762d3496a853b1a3e2b76a883b2600d
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-12-06 21:46:20 +0530

    banglore housing data

--- pandas/air-bnb-data/sol.py
+++ pandas/air-bnb-data/sol.py
@@ -23,23 +23,11 @@ print(df.price.describe())
 print("\n")
 
 # Calculate the 25th and 75th percentiles
-per_25 = df.price.quantile(0.25)
-per_75 = df.price.quantile(0.75)
+per_25 = df.price.quantile(0.01)
+per_75 = df.price.quantile(0.99)
 
 # Filter out outliers based on the percentiles
 df_no_outlier = df[(df.price > per_25) & (df.price < per_75)]
-
-# Specify the path where you want to save the CSV file
-csv_file_path = "./archive/output_file.csv"
-
-# Check if the file already exists and delete it
-if os.path.exists(csv_file_path):
-    os.remove(csv_file_path)
-    print(f"Existing file '{csv_file_path}' deleted.")
-    print("\n")
-
-# Write the DataFrame without outliers to a CSV file
-df_no_outlier.to_csv(csv_file_path, index=False)
 
 df_no_outlier["zscore"] = (
     df_no_outlier["price"] - df_no_outlier["price"].mean()
@@ -49,10 +37,6 @@ df_no_outlier["zscore"] = (
 df_no_outlier = df_no_outlier[
     (df_no_outlier["zscore"] > -2) & (df_no_outlier["zscore"] < 2)
 ]
-
-# normal distribution
-sn.histplot(data=df_no_outlier["price"], kde=True)
-plt.show()
 
 # Display lengths of the original and filtered DataFrames
 print(
@@ -73,3 +57,19 @@ print("\n")
 print("Descriptive statistics of 'price' column in the filtered DataFrame:")
 print(df_no_outlier.price.describe())
 print("\n")
+
+# Specify the path where you want to save the CSV file
+csv_file_path = "./archive/output_file.csv"
+
+# Check if the file already exists and delete it
+if os.path.exists(csv_file_path):
+    os.remove(csv_file_path)
+    print(f"Existing file '{csv_file_path}' deleted.")
+    print("\n")
+
+# Write the DataFrame without outliers to a CSV file
+df_no_outlier.to_csv(csv_file_path, index=False)
+
+# normal distribution
+sn.histplot(data=df_no_outlier["price"], kde=True)
+plt.show()
---
+++ pandas/banglore_property_data/sol.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import os
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+# Exercise
+excel_file_path = "./bhp.csv"
+df = pd.read_csv(excel_file_path)
+
+# Display the 01th and 99th percentiles of the 'price_per_sqft' column
+print("01th and 99th percentiles of 'price_per_sqft' column:")
+print(df.price.quantile([0.01, 0.99]))
+print("\n")
+
+# Display descriptive statistics of the 'price' column
+print("Descriptive statistics of 'price' column:")
+print(df.describe())
+print("\n")
+
+# Calculate the 01th and 99th percentiles
+per_01 = df.price_per_sqft.quantile(0.01)
+per_99 = df.price_per_sqft.quantile(0.99)
+
+# Filter out outliers based on the percentiles
+df_no_outlier = df[(df.price_per_sqft > per_01) & (df.price_per_sqft < per_99)]
+
+df_no_outlier["zscore"] = (
+    df_no_outlier["price_per_sqft"] - df_no_outlier["price_per_sqft"].mean()
+) / df_no_outlier["price_per_sqft"].std()
+
+# removing outlier using zscore
+df_no_outlier = df_no_outlier[
+    (df_no_outlier["zscore"] > -2) & (df_no_outlier["zscore"] < 2)
+]
+
+# Display lengths of the original and filtered DataFrames
+print(
+    f"Original DataFrame length: {len(df)}, Filtered DataFrame length: {len(df_no_outlier)}"
+)
+print("\n")
+
+# Display standard deviations of the 'price_per_sqft' column in both DataFrames
+print(
+    f"Standard deviation of 'price_per_sqft' column in the original DataFrame: {df['price_per_sqft'].std()}"
+)
+print(
+    f"Standard deviation of 'price_per_sqft' column in the filtered DataFrame: {df_no_outlier['price_per_sqft'].std()}"
+)
+print("\n")
+
+# Display descriptive statistics of the 'price_per_sqft' column in the filtered DataFrame
+print("Descriptive statistics of 'price_per_sqft' column in the filtered DataFrame:")
+print(df_no_outlier.price_per_sqft.describe())
+print("\n")
+
+# Specify the path where you want to save the CSV file
+csv_file_path = "./output_file.csv"
+
+# Check if the file already exists and delete it
+if os.path.exists(csv_file_path):
+    os.remove(csv_file_path)
+    print(f"Existing file '{csv_file_path}' deleted.")
+    print("\n")
+
+# Write the DataFrame without outliers to a CSV file
+df_no_outlier.to_csv(csv_file_path, index=False)
+
+# normal distribution
+sn.histplot(data=df_no_outlier["price_per_sqft"], kde=True)
+plt.show()
