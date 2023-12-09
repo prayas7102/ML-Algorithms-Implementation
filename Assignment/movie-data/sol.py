6a7465a47269c7151a5918c1e300facb1039d3fd
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-12-09 23:07:07 +0530

    modifies_z_score

--- Assignment/movie-data/sol.py
+++ Assignment/movie-data/sol.py
@@ -4,29 +4,39 @@ import os
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-# Exercise
-excel_file_path = "./movie_revenues.csv"
-df = pd.read_csv(excel_file_path)
-
-df["revenue"] = df["revenue"] / 10000
-std_deviation = 3.5
-print(df.describe())
 
 def get_MAD(s, median):
     diff = abs(s - median)
     return np.median(diff)
 
-median = np.median(df["revenue"])
-MAD = get_MAD(df["revenue"], median)
-
 def get_modified_z_score(x, median, MAD):
     return 0.6745 * (x - median) / MAD
 
+# Exercise
+excel_file_path = "./movie_revenues.csv"
+df = pd.read_csv(excel_file_path)
+
+df["revenue"] = df["revenue"] / 1000000
+lower_quantile = 0.25
+higher_quantile = 0.75
+std_deviation = 3.5
+print(df["revenue"].std())
+
+# Calculate the 01th and 99th percentiles
+per_01 = df["revenue"].quantile(lower_quantile)
+per_99 = df["revenue"].quantile(higher_quantile)
+
+# Filter out outliers based on the percentiles
+df = df[(df["revenue"] > per_01) & (df["revenue"] < per_99)]
+print(df["revenue"].std())
+
+median = np.median(df["revenue"])
+MAD = get_MAD(df["revenue"], median)
+
 # removing outlier using zscore
 df['modifies_z_score'] = get_modified_z_score(df["revenue"], median, MAD)
-df_no_outlier = df[
-     (df["modifies_z_score"] < std_deviation)
-]
+df_no_outlier = df[(df["modifies_z_score"] < std_deviation)]
+print(df_no_outlier["revenue"].std())
 
 # Specify the path where you want to save the CSV file
 csv_file_path = "./output_file.csv"
