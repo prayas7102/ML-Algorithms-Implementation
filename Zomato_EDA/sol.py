074ac6200d1b4185ed07941f187f58837cafb301
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-12-19 10:03:49 +0530

    # countries where rating color is white or has given zero ratings

--- Zomato_EDA/sol.py
+++ Zomato_EDA/sol.py
@@ -10,6 +10,7 @@ df = pd.read_csv(excel_file_path, encoding="latin-1")
 # print(df.head())
 # print(df.columns)
 # print(df.info())
+# finding null values in dataframe
 # print(df.isnull())
 # print(df.isnull().sum())
 # print(df.isnull().sum()["Cuisines"])
@@ -17,12 +18,12 @@ df = pd.read_csv(excel_file_path, encoding="latin-1")
 # print(df.dtypes)
 # sns.heatmap(df.isnull())
 # plt.show()
-feat_with_null_val = [feat for feat in df.columns if df.isnull().sum()[feat] > 0]
+feat_with_null_val = [feat for feat in df.columns if df.isnull()[feat].sum() > 0]
 # print(feat_with_null_val)
 df_country = pd.read_excel("./Country-Code.xlsx")
 # print(df_country.head())
 df_merged = pd.merge(df, df_country, on="Country Code", how="left")
-print(df_merged.head())
+# print(df_merged.head())
 # print(df_merged.Country.value_counts())
 country_val = df_merged.Country.value_counts().values
 country_name = df_merged.Country.value_counts().index
@@ -34,8 +35,11 @@ ratings = (
     .reset_index() 
     .rename(columns={0: "Rating Count"})
 )
-print(ratings.head())
-# zero_rating = [countree for countree in df_merged["Country"] if df_merged["Aggregate rating"]==0]
-print(zero_rating)
+# print(ratings.head())
+# countries where rating color is white or has given zero ratings
+# reset_index to Converting Grouped Data to DataFrame:
+zero_rating = df_merged[df_merged["Rating color"] == "White"].groupby("Country").size().reset_index()
+# print(zero_rating)
+# 
 sns.barplot(data=ratings, x="Aggregate rating", y="Rating Count", hue="Rating color")
 plt.show()
