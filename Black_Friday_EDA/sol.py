072ad6f214479d6bf145efbc699cdd55ad76d563
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-12-21 09:31:46 +0530

    ipynb

--- Black_Friday_EDA/sol.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# %matplotlib inline
-
-excel_file_path = "./black_friday/train.csv"
-df = pd.read_csv(excel_file_path, encoding="latin-1")
